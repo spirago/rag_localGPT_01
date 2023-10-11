c4fcd3ad323ab06b97b72558b75546949a63ba11
petcebch <peter.cebula@spirago.ch>
2023-10-11 23:22:13 +0200

    start.sh added

---
+++ start.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+echo "pod started"
+
+if [[ $PUBLIC_KEY ]]
+then
+    mkdir -p ~/.ssh
+    chmod 700 ~/.ssh
+    cd ~/.ssh
+    echo $PUBLIC_KEY >> authorized_keys
+    chmod 700 -R ~/.ssh
+    cd /
+    service ssh start
+fi
+
+# source /root/miniconda3/etc/profile.d/conda.sh  # Adjust this path based on where Miniconda is installed
+# conda activate privategpt
+
+# python ingest.py (--device_type cpu)
+# python run_localGPT.py --show_sources (--device_type cpu/mps)
+sleep infinity
